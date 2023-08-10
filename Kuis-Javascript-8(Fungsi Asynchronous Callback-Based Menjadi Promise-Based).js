4eb9ccb0fc9d374b7795c3090f918fd34f41b1cc
Yuda Hasibuan <50428419+YudaHasibuan23@users.noreply.github.com>
2023-08-10 16:56:20 +0800

    Create Kuis-Javascript-8(Fungsi Asynchronous Callback-Based Menjadi Promise-Based).js

---
+++ Kuis-Javascript-8(Fungsi Asynchronous Callback-Based Menjadi Promise-Based).js
@@ -0,0 +1,33 @@
+**
+ * @TODO
+ * Ubahlah fungsi asynchronous callback-based getProvinces menjadi Promise-based.
+ *
+ * Catatan:
+ * - Anda boleh menggunakan util.promisify untuk mengubah fungsi callback-based menjadi Promise-based.
+ * - Jika nama fungsinya berubah, sesuaikan nilai yang diekspor tanpa mengubah nama properti di akhir berkas ini.
+ */
+
+const { promisify } = require('util');
+
+function getProvinces(countryId, callback) {
+  setTimeout(() => {
+    if (countryId === 'id') {
+      callback(null, [
+        { id: 'id-jk', name: 'Jakarta' },
+        { id: 'id-bt', name: 'Banten' },
+        { id: 'id-jr', name: 'Jawa Barat' },
+      ]);
+      return;
+    }
+
+    callback(new Error('Country not found'), null);
+  }, 1000);
+}
+
+const getProvincesPromise = promisify(getProvinces);
+
+getProvincesPromise('id')
+  .then(provinces => console.log(provinces))
+  .catch(err => console.log(err.message));
+
+module.exports = { getProvinces: getProvincesPromise };
